c32c0fb6274acbf9bfac3baaf78a3e14bb38e951
tusmanakhter <tusman96@hotmail.com>
2019-05-11 01:36:58 -0400

    Added core functionality
    - Add sender message
    - Add reciever message
    - Add date divider

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MoodleChatFaker
+
+Simple website to generate Moodle chats. Can be used to fool your friends. 
+
+This is not intended to be used maliciously.
+
+## TODO
+
+- Validation of fields
+- Clean up css duplication
+- Add download image button
+- Add delete & edit functionality
---
+++ main.js
@@ -0,0 +1,47 @@
+function autoExpandTextArea() {
+    if (event.target.clientHeight < event.target.scrollHeight && event.target.rows < 5) {
+      const chat = document.querySelector("#chat-bubble-area");
+      chatHeight = parseInt(window.getComputedStyle(chat).height);
+      chatHeight -= 20;
+      chat.style.height = chatHeight;
+      event.target.rows += 1
+    }
+}
+
+function displayName() {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("name").innerHTML = name;
+}
+
+function addMessage(type) {
+  if (type == "sent") {
+    var className = "chat-bubble-right"
+  } else if (type == "recieved") {
+    var className = "chat-bubble-left"
+  }
+
+  const message = document.getElementById("message").value;
+  let time = document.getElementById("time").value;
+  time = new moment(time, "HH:mm")
+  time = time.format("hh:mm A")
+
+  document.getElementById("chat-bubble-area").insertAdjacentHTML('beforeend',
+    '<div class="chat-bubble-container">\
+      <div class="' + className + '">\
+        <p class="chat">'
+          + message +
+        '</p> <span class="time">' + time + '</span>\
+      </div>\
+    </div>'
+  )
+}
+
+function addDateDivider() {
+  let date = document.getElementById("date").value;
+  date = new moment(date);
+  date = date.format("dddd, D MMMM YYYY");
+
+  document.getElementById("chat-bubble-area").insertAdjacentHTML('beforeend',
+    '<div class="date">' + date + '</div>'
+  )
+}
